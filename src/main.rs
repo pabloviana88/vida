63c276c5c98e37aae06c12bfc46642cc54ba5435
pabloviana88 <pablo1_2@hotmail.com>
2021-02-02 17:10:07 -0600

    version 1

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+fn sentido_de_la_vida() -> String{
+    "Hacerla chillar".to_string()
+}
+
+fn cuando_inicia_senectud() -> i32 {
+    27
+}
+
+fn main() {
+    //println!("Hola amigos!!!");
+    println!("El sentido de la vida es {}",sentido_de_la_vida());
+    println!("La senectitud incia a los {}", cuando_inicia_senectud());
+
+}
